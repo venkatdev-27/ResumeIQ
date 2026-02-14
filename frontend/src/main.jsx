67ffa4614810f132ed53611d5e7cf4ebf3325b3e
venkatdev-27 <k.venky5678@gmail.com>
2026-02-14 19:06:04 +0530

    chore(router): enable v7 relative splat future flag

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -10,7 +10,7 @@ import store from './redux/store';
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <Provider store={store}>
-            <BrowserRouter>
+            <BrowserRouter future={{ v7_relativeSplatPath: true }}>
                 <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
                     <App />
                 </ThemeProvider>
